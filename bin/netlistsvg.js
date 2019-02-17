acdd2c7105e4b1e384527d87fb4612c21995cddd
ito akira <scwirq@gmail.com>
2019-02-18 08:16:50 +0900

    modify the src to show the text of each module.

--- bin/netlistsvg.js
+++ bin/netlistsvg.js
@@ -9,26 +9,28 @@ var lib = require('../lib'),
 
 if (require.main === module) {
     var argv = yargs
-        .demand(1)
-        .usage('usage: $0 input_json_file [-o output_svg_file] [--skin skin_file]')
+        .demand(0)
+        .usage('usage: $0 [--skin skin_file]')
         .argv;
 
-    main(argv._[0], argv.o, argv.skin);
+    main(argv.skin);
 }
 
-function main(netlistpath, outputPath, skinPath) {
+function main(skinPath) {
     skinPath = skinPath || path.join(__dirname, '../lib/default.svg');
-    outputPath = outputPath || 'out.svg';
+    skinPath = path.join(__dirname, '../lib/default.svg');
+    //outputPath = outputPath || 'out.svg';
     fs.readFile(skinPath, 'utf-8', function(err, skin_data) {
         if (err) throw err;
-        fs.readFile(netlistpath, function(err, netlist_data) {
+        fs.readFile('/dev/stdin', function(err, netlist_data) {
             if (err) throw err;
             var netlist = json5.parse(netlist_data);
             lib.render(skin_data, netlist, function(err, svg_data) {
                 if (err) throw err;
-                fs.writeFile(outputPath, svg_data, 'utf-8', function(err) {
-                    if (err) throw err;
-                });
+                //fs.writeFile(outputPath, svg_data, 'utf-8', function(err) {
+                //    if (err) throw err;
+                //});
+                console.info(svg_data);
             });
         });
     });
